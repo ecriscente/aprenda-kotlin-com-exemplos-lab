6f3dadd2b04ace90f8acc50c8385927f01420495
Erion Criscente <erioncriscente@gmail.com>
2023-09-18 18:50:41 -0300

    minha solução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Aluno(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+    val inscritos = mutableListOf<Aluno>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricularAluno(aluno: Aluno) {
+        inscritos.add(aluno)
+        println("Aluno ${aluno.nome} matriculado na formação $nome")
+    }
+
+    fun matricularAlunos(alunos: List<Aluno>) {
+        inscritos.addAll(alunos)
+        alunos.forEach { aluno ->
+            println("Aluno ${aluno.nome} matriculado na formação $nome")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin", 120)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento Android com Kotlin", 180)
+
+    // Criando uma formação
+    val formacaoKotlin = Formacao("Formação Kotlin", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
+
+    // Criando alguns alunos
+    val aluno1 = Aluno("João")
+    val aluno2 = Aluno("Maria")
+
+    // Matriculando os alunos na formação
+    formacaoKotlin.matricularAluno(aluno1)
+    formacaoKotlin.matricularAluno(aluno2)
+
+    // Lista de alunos para matricular de uma vez
+    val novosAlunos = listOf(Aluno("Pedro"), Aluno("Laura"), Aluno("Lucas"))
+    formacaoKotlin.matricularAlunos(novosAlunos)
+    
+    for (aluno in formacaoKotlin.inscritos) {
+        println(aluno.nome)
+    }
+    
+    println(formacaoKotlin.nivel)
+    println(formacaoKotlin.conteudos)
 }
